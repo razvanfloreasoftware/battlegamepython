96e650dd2b02a7bf0e171763bbd9d126bacb09e7
razvanfloreasoftware <77797609+razvanfloreasoftware@users.noreply.github.com>
2021-01-31 21:07:14 +0000

    3rd commit - repairing error

--- main.py
+++ main.py
@@ -85,7 +85,6 @@ while running:
         item = player.items[item_choice]["item"]
         player.items[item_choice]["quantity"] -= 1
 
-
         if item.type == "potion":
             player.heal(item.prop)
             print(bcolors.OKGREEN + "\n" + item.name + " heals for", str(item.prop), "HP" + bcolors.ENDC)
